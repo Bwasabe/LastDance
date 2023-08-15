e1c9e36688c8bbe4ed6f8a8b861b31a6c3f25b7f
Bwasabe <jixg20@gmail.com>
2023-08-16 00:46:25 +0900

    Sliding 스크립트만 추가

---
+++ Assets/01.Scripts/Player/PlayerSliding.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSliding : PlayerComponentBase
+{
+    [SerializeField]
+    private float _slidingForce;
+
+    private void Update()
+    {
+        
+    }
+    
+}
