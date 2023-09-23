b93a5d142ddc0491de1af756ef6f50e9c3cb0b76
Bwasabe <jixg20@gmail.com>
2023-09-24 02:38:50 +0900

    Setting 삭제, 웅크리기, 슬라이딩 Collider 범위 수정

--- Assets/01.Scripts/Player/PlayerCrouch.cs
+++ Assets/01.Scripts/Player/PlayerCrouch.cs
@@ -7,55 +7,93 @@ using UnityEngine;
 public class PlayerCrouch : PlayerComponentBase
 {
     [field:SerializeField]
-    public float CrouchSpeed{ get; private set; }= 3f;
+    public float CrouchSpeed{ get; private set; } = 3f;
+
+    [SerializeField]
+    private float _capsuleHeight = 1f;
+
+    [SerializeField]
+    private Vector3 _capsuleCenter = new(0f, .5f, 0f);
     
     [SerializeField]
     private float _camHeight = 0.5f;
     [SerializeField]
-    private float _camMoveDuration = 0.8f;
+    private float _animDuration = 0.15f;
 
     private PlayerMove _playerMove;
     private PlayerGroundController _groundController;
+
+    private CapsuleCollider _capsuleCollider;
     
     
     private Transform _camTransform;
 
-    private Tweener _camMoveTweener;
+    private Sequence _sequence;
+
+    private Vector3 _capsuleOriginCenter;
     
+    private float _capsuleOriginHeight;
     private float _camOriginHeight;
 
     protected override void Start()
     {
         _camTransform = Define.MainCam.transform;
-
-        _camOriginHeight = _camTransform.localPosition.y;
         
-        
+        _capsuleCollider = transform.GetComponentCache<CapsuleCollider>();
         _playerMove = transform.GetComponentCache<PlayerMove>();
         _groundController = transform.GetComponentCache<PlayerGroundController>();
+        
+        _camOriginHeight = _camTransform.localPosition.y;
+        _capsuleOriginHeight = _capsuleCollider.height;
+        _capsuleOriginCenter = _capsuleCollider.center;
         
         base.Start();
     }
 
     private void StartCrouch()
     {
-        _camMoveTweener.Kill();
+        if(_sequence.IsActive() && _sequence.IsPlaying())
+            _sequence.Kill();
         
         Ease ease = Ease.InQuart;
         if(Define.GetInput() == Vector3.zero)
-        {
             ease = Ease.Linear;
-        }
-        
-        _camMoveTweener = _camTransform.DOLocalMoveY(_camHeight, _camMoveDuration).SetEase(ease);
 
+        _sequence = DOTween.Sequence();
+        
+        _sequence.Join(DOTween.To(
+            () => _capsuleCollider.center,
+            value => _capsuleCollider.center = value,
+            _capsuleCenter, _animDuration));
+        
+        _sequence.Join(DOTween.To(
+            () => _capsuleCollider.height,
+            value => _capsuleCollider.height = value,
+            _capsuleHeight, _animDuration));
+
+        _sequence.Join(_camTransform.DOLocalMoveY(_camHeight, _animDuration).SetEase(ease));
+        
         _playerMove.Speed = CrouchSpeed;
     }
 
     private void EndCrouch()
     {
-        _camMoveTweener.Kill();
-        _camMoveTweener = _camTransform.DOLocalMoveY(_camOriginHeight, _camMoveDuration).SetEase(Ease.Linear);
+        if(_sequence.IsActive())
+            _sequence.Kill();
+
+        _sequence = DOTween.Sequence();
+        
+        _sequence.Join(DOTween.To(
+            () => _capsuleCollider.center,
+            value => _capsuleCollider.center = value,
+            _capsuleOriginCenter, _animDuration));
+        
+        _sequence.Join(DOTween.To(
+            () => _capsuleCollider.height,
+            value => _capsuleCollider.height = value,
+            _capsuleOriginHeight, _animDuration));
+
+        _sequence.Join(_camTransform.DOLocalMoveY(_camOriginHeight, _animDuration).SetEase(Ease.Linear));
         
         _playerMove.ResetSpeed();
     }
